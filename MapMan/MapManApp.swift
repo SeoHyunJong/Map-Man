cded8ab71d43d68895bb0dfce74d16a28d62359d
SeoHyunJong <shj971219@gmail.com>
2022-09-01 20:57:29 +0900

    Initial Commit

---
+++ MapMan/MapManApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapManApp.swift
+//  MapMan
+//
+//  Created by 서현종 on 2022/09/01.
+//
+
+import SwiftUI
+
+@main
+struct MapManApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
